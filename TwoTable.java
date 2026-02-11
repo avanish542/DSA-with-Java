618dc7c991846fcf26127b949ca45c58ca55bc7d
Avanish542 <avanishvishwakarma25@gmail.com>
2026-02-11 22:33:25 +0530

    day-1

---
+++ TwoTable.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TwoTable {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<= 10; i++){
+            System.out.println("Table of any n Number is :" + i*n);
+        };
+        System.out.println();
+    }
+}
