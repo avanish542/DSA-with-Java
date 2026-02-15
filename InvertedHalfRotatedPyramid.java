2ab78f5917da9552db67f8f0231aaea2b57bc0ce
Avanish542 <avanishvishwakarma25@gmail.com>
2026-02-15 21:25:03 +0530

    day-5 b

---
+++ InvertedHalfRotatedPyramid.java
@@ -0,0 +1,19 @@
+// Print Inverted Half Roted Pyramid 
+import java.util.*;
+public class InvertedHalfRotatedPyramid {
+    public static void InvertedHalf_RotatedPyramid(int n){
+        for(int i = 1; i<= n; i++){
+            for(int j = 1; j<=n-1; j++ ){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<= i; j++){
+            System.out.print("*");
+            }
+              System.out.println();
+        }       
+      
+    }
+    public static void main(String args[]){
+        InvertedHalf_RotatedPyramid(5);
+    }
+}
