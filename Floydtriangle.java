d4e1ec3307cb4c3237e2bbe8aef37a8c17539a3a
Avanish542 <avanishvishwakarma25@gmail.com>
2026-02-17 21:38:35 +0530

    Day-7

--- Floydtriangle.java
+++ Floydtriangle.java
@@ -1,17 +1,39 @@
+// import java.util.*;
+// public class Floydtriangle {
+//     public static void PrintFloyd_triangle(int n){
+//         int counter = 1;
+//         for(int i = 1; i<=n; i++){
+//             for( int j = 1; j<=i; j++){
+//                 System.out.print(counter + " ");
+//                 counter++;
+//             }
+//             System.out.println();
+//         }
+//     }
+//      public static void main(String args[]) {
+//         PrintFloyd_triangle(7);
+//     }
+
+// }
+
+
+
+// Floyd Triangle with user input
 import java.util.*;
 public class Floydtriangle {
-    public static void PrintFloyd_triangle(int n){
+    public static void Floyd_triangle(int n) {
         int counter = 1;
-        for(int i = 1; i<=n; i++){
-            for( int j = 1; j<=i; j++){
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j<=i; j++){
                 System.out.print(counter + " ");
                 counter++;
             }
-            System.out.println();
-        }
+            System.out.println();           
+        }       
     }
-     public static void main(String args[]) {
-        PrintFloyd_triangle(7);
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Floyd_triangle(n);
     }
-
 }
