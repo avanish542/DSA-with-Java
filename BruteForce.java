3373e3fc6aa7ef05c6a84bc954b1567eab05efca
Avanish542 <avanishvishwakarma25@gmail.com>
2026-02-24 22:55:51 +0530

    day-13

---
+++ BruteForce.java
@@ -0,0 +1,27 @@
+// Max Subarray sum
+import java.util.*;
+public class BruteForce {
+    public static void maxSubarraysum(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for(int j = 1; j< numbers.length; j++){
+                int end = j;
+                currSum = 0;
+                for(int k = start; k<= end; k++){
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if(maxSum < currSum){
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("maxSum=" +maxSum);
+    }
+    public static void main(String args[]){
+        int numbers[] = {2, 4, 6, 8, 10};
+        maxSubarraysum(numbers);
+    }
+}
