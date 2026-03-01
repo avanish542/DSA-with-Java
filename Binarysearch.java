f8df46411c9719fee51b36d64baab5d249d72e8d
Avanish542 <avanishvishwakarma25@gmail.com>
2026-03-01 14:24:57 +0530

    Day-13

--- Binarysearch.java
+++ Binarysearch.java
@@ -1,25 +1,52 @@
+// import java.util.*;
+// public class Binarysearch{
+//     public static int getbinary(int numbers[], int key){
+//         int start = 0;
+//          int end = numbers.length-1;
+//         while(start <= end){
+//             int mid = (start+end)/2;
+//             if(numbers[mid] == key){
+//                 return mid;
+//             }
+//             if(numbers[mid] <= key){
+//                 start = mid+1;
+//             }else{
+//                 end = mid-1;
+//             }
+
+//         }
+//         return -1;
+//     }
+//     public static void main(String args[]){
+//         int numbers[] = {2,3,5,7,10,13,14,17};
+//         int key = 10;
+//         System.out.println("The key is at index:" + getbinary(numbers, key));
+//     }
+// }
+
+
 import java.util.*;
-public class Binarysearch{
-    public static int getbinary(int numbers[], int key){
-        int start = 0;
-         int end = numbers.length-1;
+public class Binarysearch {
+    public static int getbinary(int numbers[] ,int key) {
+        int start = 0; 
+        int end = numbers.length - 1;
         while(start <= end){
             int mid = (start+end)/2;
             if(numbers[mid] == key){
                 return mid;
             }
             if(numbers[mid] <= key){
-                start = mid+1;
-            }else{
-                end = mid-1;
+                 start = mid+1;
+            }else {
+                 end = mid-1;
             }
-
+            
         }
         return -1;
     }
     public static void main(String args[]){
-        int numbers[] = {2,3,5,7,10,13,14,17};
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 25};
         int key = 10;
-        System.out.println("The key is at index:" + getbinary(numbers, key));
+        System.out.println("The key is at index:" +getbinary( numbers ,  key));
     }
 }
