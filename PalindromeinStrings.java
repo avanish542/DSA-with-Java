10f9f8525be7ce264f37ac58c6ef3eca3178d732
Avanish542 <avanishvishwakarma25@gmail.com>
2026-03-03 12:42:56 +0530

    Day-15 b

---
+++ PalindromeinStrings.java
@@ -0,0 +1,21 @@
+//  Check if a String is a Palindrome
+
+import java.util.*;
+public class PalindromeinStrings {
+    public static boolean isPalindrome(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
+
+// Output
+// true
---
+++ Strings.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+public class Strings {
+    public static void main(String args[]){
+        // char arr[] = {'a','b', 'c','d'};
+        // String str = "abcd";
+        // String str2 = new String("xyz");
+
+        // // Inpute / OUtput
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine();
+        // System.out.println(name);
+
+        // Length function
+        // String fullname = "Avanish Vishwakarma";
+        // System.out.println(fullname.length());
+
+        // Concatenation
+        String firstname = "Avanish";
+        String lastname  = "Vishwakarma";
+        String fullname = firstname + " " +lastname;
+        System.out.println(fullname);
+        System.out.println(fullname.charAt(0));
+    }
+}
+
+// String Are Immutable in nature
+// Above three are the method of declaring string
+// In string Length is a Function: .length();
+
+
